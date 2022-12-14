7b60009f987d15d4abc3fcee23765db0c979ea99
John Torres <johnnyjamestorres@outlook.com>
2022-12-14 13:52:17 -0800

    Created functions for each mode

--- nvim/lua/remaps.lua
+++ nvim/lua/remaps.lua
@@ -1,31 +1,34 @@
-local opts = { noremap = true, silent = true }
-
-local remappings = {
-  i = {
-    -- Move current line up/down
-    ["<A-j>"] = "<Esc>:m .+1<CR>==gi",
-    ["<A-k>"] = "<Esc>:m .-2<CR>==gi",
-  },
-  n = {
-    -- Move current line up/down
-    ["<A-j>"] = ":m .+1<CR>==",
-    ["<A-k>"] = ":m .-2<CR>==",
-    ["<leader>,"] = ":write<CR>",
-  },
-  v = {
-    -- Better indenting
-    ["<"] = "<gv",
-    [">"] = ">gv",
-  },
-  x = {
-    -- Move current block up/down
-    ["<A-j>"] = ":m '>+1<CR>gv-gv",
-    ["<A-k>"] = ":m '<-2<CR>gv-gv",
-  },
-}
-
-for mode, remaps in pairs(remappings) do
-  for lhs, rhs in pairs(remaps) do
+local function bind(mode, outer_opts)
+  outer_opts = outer_opts or { noremap = true, silent = true }
+  return function(lhs, rhs, opts)
+    opts = vim.tbl_extend("force", 
+      outer_opts,
+      opts or {}
+    )
     vim.keymap.set(mode, lhs, rhs, opts)
   end
 end
+
+local nnoremap = bind("n")
+local inoremap = bind("i")
+local vnoremap = bind("v")
+local xnoremap = bind("x")
+
+
+-- Move lines up/down
+nnoremap("<A-j>", ":m .+1<CR>==")
+nnoremap("<A-k>", ":m .-2<CR>==")
+-- Write file
+nnoremap("<leader>,", ":write<CR>")
+
+-- Move lines up/down
+inoremap("<A-j>", "<Esc>:m .+1<CR>==gi")
+inoremap("<A-k>", "<Esc>:m .-2<CR>==gi")
+
+-- Keep selection when indenting
+vnoremap("<", "<gv")
+vnoremap(">", ">gv")
+
+-- Move lines up/down
+xnoremap("<A-j>", ":m '>+1<CR>gv-gv")
+xnoremap("<A-k>", ":m '<-2<CR>gv-gv")
