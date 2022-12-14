863453a6a8c629d767c5b9a5e3b79fcc4536da78
John Torres <johnnyjamestorres@outlook.com>
2022-12-14 11:52:43 -0800

    Add smartcase, smartindent, disable wrap

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,8 +1,8 @@
 local default_options = {
   backup = false,
   expandtab = true,
-  fileencoding = "utf-8",
-  guicursor = "",
+  fileencoding = 'utf-8',
+  guicursor = '',
   hidden = true,
   hlsearch = true,
   incsearch = true,
@@ -11,11 +11,14 @@ local default_options = {
   scrolloff = 8,
   shiftwidth = 2,
   sidescrolloff = 8,
+  smartcase = true,
+  smartindent = true,
   softtabstop = 2,
   splitbelow = true,
   splitright = true,
   tabstop = 2,
   title = true,
+  wrap = false,
 }  
 
 for k, v in pairs(default_options) do
