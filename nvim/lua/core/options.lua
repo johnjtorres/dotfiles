8c567f11ca39bbb5ba76ff7902b595ddb5005de6
John Torres <johnnyjamestorres@outlook.com>
2022-12-15 12:06:54 -0800

    Increase number column width to 4

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,17 +1,20 @@
 local default_options = {
 	backup = false,
+	cursorline = true,
 	expandtab = true,
 	fileencoding = "utf-8",
 	guicursor = "",
 	hidden = true,
 	hlsearch = true,
-	laststatus = 3,
 	incsearch = true,
+	laststatus = 3,
 	number = true,
+	numberwidth = 4,
 	relativenumber = true,
 	scrolloff = 8,
 	shiftwidth = 2,
 	sidescrolloff = 8,
+	signcolumn = "yes",
 	smartcase = true,
 	smartindent = true,
 	softtabstop = 2,
@@ -20,6 +23,7 @@ local default_options = {
 	tabstop = 2,
 	title = true,
 	wrap = false,
+	swapfile = false,
 }
 
 for k, v in pairs(default_options) do
