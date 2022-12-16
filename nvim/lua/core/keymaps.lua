6055d3b6bf16a6aff3692eec3ff57d48f1544621
John Torres <johnnyjamestorres@outlook.com>
2022-12-16 10:08:19 -0800

    Setup LSP and autoformatting

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,6 +1,62 @@
+-- Telescope
 local telescope = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", telescope.find_files, {})
 vim.keymap.set("n", "<leader>fg", telescope.live_grep, {})
 vim.keymap.set("n", "<leader>fb", telescope.buffers, {})
 vim.keymap.set("n", "<C-p>", telescope.git_files, {})
 vim.keymap.set("n", "<leader>fh", telescope.help_tags, {})
+
+-- Save
+vim.keymap.set("n", "<leader>,", vim.cmd.write)
+
+-- Launch netrw
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+-- Move selected lines up/down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Append line below to current line
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Cursor stays centered when jumping half a page
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Searched items stay centered on screen
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Paste over word without losing copied text
+vim.keymap.set("x", "<leader>p", '"_dP')
+
+-- Copy text to system clipboard
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+
+-- Delete text and send to _ register
+vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set("v", "<leader>d", '"_d')
+
+-- Unbind Q
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<leader>f", function()
+	vim.lsp.buf.format()
+end)
+
+-- Quick fix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Replace word under cursor
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+
+-- Remove trailing whitespace
+vim.keymap.set("n", "<leader>tw", ":%s/\\s\\+$//e<CR>")
+
+-- Insert lines
+vim.keymap.set("n", "<leader>o", "o<Esc>k")
+vim.keymap.set("n", "<leader>O", "O<Esc>j")
