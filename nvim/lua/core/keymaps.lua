6b40d5c6af7ef7481ca00554947e3280ff363441
John Torres <johnnyjamestorres@outlook.com>
2022-12-16 06:44:34 -0800

    Add keymaps for telescope

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -67,6 +67,8 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
+vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>", {})
 
 -- Move lines up/down
 inoremap("<A-j>", "<Esc>:m .+1<CR>==gi")
