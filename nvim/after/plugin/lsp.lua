28b44ac16ab7e6bded521c7f15da62b4de3e1056
John Torres <johnnyjamestorres@outlook.com>
2022-12-16 11:47:16 -0800

    Modify autocompletion keybindings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,53 +1,85 @@
 local lsp = require("lsp-zero")
+local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  "eslint",
-  "sumneko_lua",
+	"eslint",
+	"sumneko_lua",
 })
 
 lsp.set_preferences({
-  sign_icons = {},
+	sign_icons = {},
 })
 
+local has_words_before = function()
+	unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings,
+	mapping = {
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	},
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr, remap = false }
+	local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", function()
-    vim.lsp.buf.definition()
-  end, opts)
-  vim.keymap.set("n", "K", function()
-    vim.lsp.buf.hover()
-  end, opts)
-  vim.keymap.set("n", "<leader>vws", function()
-    vim.lsp.buf.workspace_symbol()
-  end, opts)
-  vim.keymap.set("n", "<leader>vd", function()
-    vim.diagnostic.open_float()
-  end, opts)
-  vim.keymap.set("n", "[d", function()
-    vim.diagnostic.goto_next()
-  end, opts)
-  vim.keymap.set("n", "]d", function()
-    vim.diagnostic.goto_prev()
-  end, opts)
-  vim.keymap.set("n", "<leader>vca", function()
-    vim.lsp.buf.code_action()
-  end, opts)
-  vim.keymap.set("n", "<leader>vrr", function()
-    vim.lsp.buf.references()
-  end, opts)
-  vim.keymap.set("n", "<leader>vrn", function()
-    vim.lsp.buf.rename()
-  end, opts)
-  vim.keymap.set("i", "<C-h>", function()
-    vim.lsp.buf.signature_help()
-  end, opts)
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>vws", function()
+		vim.lsp.buf.workspace_symbol()
+	end, opts)
+	vim.keymap.set("n", "<leader>vd", function()
+		vim.diagnostic.open_float()
+	end, opts)
+	vim.keymap.set("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	vim.keymap.set("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end, opts)
+	vim.keymap.set("n", "<leader>vca", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrr", function()
+		vim.lsp.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrn", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
 end)
 
 lsp.setup()
